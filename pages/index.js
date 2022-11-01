f498124e592a08b7a3f5273dbcb084c5eabfbb27
kenechvkwv <ubesiekene@gmail.com>
2022-11-01 01:35:15 +0100

    form content not styled

--- pages/index.js
+++ pages/index.js
@@ -60,6 +60,128 @@ export default function Home() {
             </ul>
           </nav>
 
+          {/* Modal */}
+
+          {showModal ? (
+            <>
+              <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                  {/*content*/}
+                  <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                    {/*header*/}
+                    <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                      <h3 className="text-3xl font-semibold">
+                        Make Your Enquiries
+                      </h3>
+                    </div>
+                    {/*body Form*/}
+                    <div className="relative p-6 flex-auto">
+                      <form class="w-full max-w-sm">
+                        {/* Full Name */}
+                        <div class="md:flex md:items-center mb-6">
+                          <div class="md:w-1/3">
+                            <label
+                              class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                              for="inline-full-name"
+                            >
+                              Full Name
+                            </label>
+                          </div>
+                          <div class="md:w-2/3">
+                            <input
+                              class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                              id="inline-full-name"
+                              type="text"
+                              placeholder="Jane Doe"
+                            />
+                          </div>
+                        </div>
+                        {/* Phone NUmber  */}
+                        <div class="md:flex md:items-center mb-6">
+                          <div class="md:w-1/3">
+                            <label
+                              class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                              for="phone"
+                            >
+                              Phone Number
+                            </label>
+                          </div>
+                          <div class="md:w-2/3">
+                            <input
+                              class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                              id="phone"
+                              type="tel"
+                              placeholder="+1234567890"
+                            />
+                          </div>
+                        </div>
+
+                        {/* Email  */}
+                        <div class="md:flex md:items-center mb-6">
+                          <div class="md:w-1/3">
+                            <label
+                              class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                              for="email"
+                            >
+                              Email Address
+                            </label>
+                          </div>
+                          <div class="md:w-2/3">
+                            <input
+                              class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                              id="email"
+                              type="email"
+                              placeholder="janedoe@example.com"
+                            />
+                          </div>
+                        </div>
+
+                        {/* Inquiry Text */}
+                        <div class="mb-6">
+                          <label
+                            for="large-input"
+                            class="block mb-2 text-sm font-medium text-gray-500 dark:text-gray-300"
+                          >
+                            Your Message
+                          </label>
+                          <input
+                            type="text"
+                            id="large-input"
+                            class="block p-4 w-full text-gray-700 bg-gray-200 rounded-lg border border-gray-300 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          />
+                        </div>
+
+                        {/* Submit button  */}
+                        <div class="md:flex md:items-center">
+                          <div class="md:w-1/3"></div>
+                          <div class="md:w-2/3">
+                            <button
+                              class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                              type="button"
+                            >
+                              Submit
+                            </button>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                    {/*footer*/}
+                    <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                      <button
+                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                        type="button"
+                        onClick={() => setShowModal(false)}
+                      >
+                        Close
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+            </>
+          ) : null}
+
           <div className="text-center p-10 ">
             <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl text-center hover:-translate-y-1 hover:scale-80 duration-300">
               Kene Ubesie
